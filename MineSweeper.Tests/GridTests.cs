df916e8fa36c8283b969d561095ffe710b99878d
Otakar Hirs <485661@mail.muni.cz>
2021-03-29 17:34:31 +0200

    feat: first cell tests

---
+++ MineSweeper.Tests/GridTests.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace MineSweeper.Tests
+{
+    public class GridTests
+    {
+        
+        
+        [Test]
+        public void GetCell_IsNotNull()
+        {
+            Grid g = new Grid(1);
+            var cell = g.GetCell(0, 0);
+            cell.Should().Be().NotNull();
+        }
+        
+        [Test]
+        [TestCase(4)]
+        public void GivenNewGrid_AllCellsAreNotDiscovered(int size)
+        {
+            Grid g = new Grid(size);
+            for(int row = 0; row < size; row++)
+            {
+                for(int col = 0; col < size; col++)
+                {
+                    var cell = g.GetCell(row, col);
+                    cell.IsDiscovered.Should().Be(false);
+                }
+            }
+            
+        }
+    }
+}
---
+++ MineSweeper.Tests/MinesweeperTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using FakeItEasy;
+namespace MineSweeper.Tests
+{
+    public class MinesweeperTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void P()
+        {
+            Assert.Pass();
+        }
+    }
+}
