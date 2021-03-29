c905acb1339069dbfb7fc0650943749979ff9b79
Ondřej Slimák <ondrej.slimak@riganti.cz>
2021-03-29 17:58:01 +0200

    added GetCell test

--- MineSweeper/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MineSweeper
-{
-    public class Class1
-    {
-    }
-}
---
+++ MineSweeper/Grid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MineSweeper
+{
+    public class Grid
+    {
+        private  Cell[,] _grid;
+        private int _size;
+
+        public Grid(int size)
+        {
+            _size = size;
+            _grid = new Cell[_size, _size];
+            
+            for (var row = 0; row < _size; row++)
+            for (var col = 0; col < _size; col++)
+            {
+                _grid[row, col] = new Cell();
+            }
+        }
+
+        public Cell GetCell(int x, int y)
+            => _grid[x, y];
+    }
+}
